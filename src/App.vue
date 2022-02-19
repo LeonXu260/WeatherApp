fcbb32c5a8185baa50820a411eab824b9cfc1cf3
Leon Xu <dcwinfield362@gmail.com>
2022-02-18 18:21:33 -0800

    fix: importing the navigation component directory

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,7 @@
 <script>
 import axios from "axios";
 import db from "./firebase/init";
-import Navigation from "./components/TheNavigation";
+import Navigation from "./components/Navigation";
 import Modal from "./components/Modal";
 export default {
   name: "App",
